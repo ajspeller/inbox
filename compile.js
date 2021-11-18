4ba4296cd6e311cda119156d510b1084e8f42569
AJ Speller <ajspeller@gmail.com>
2021-11-17 22:23:23 -0500

    Update Test

--- compile.js
+++ compile.js
@@ -6,3 +6,5 @@ const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
 module.exports = solc.compile(source, 1).contracts[':Inbox'];
+
+const temp = 1; // delete testing
